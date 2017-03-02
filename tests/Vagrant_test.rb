0d7d5a66893c91eef33a17c7b840bd04895b5dc0
zaterio <zaterio@grupoz.cl>
2017-03-02 11:21:12 -0300

    change cluster var for better iteration

--- tests/Vagrant_test.rb
+++ tests/Vagrant_test.rb
@@ -9,13 +9,11 @@ CephAdminNode = []
 
 cluster.each do |array|
  if array.include? 'admin'
-  CephAdminNode << array['node']
- end 
- array['network'].each do |net|
-  if net.include? 'ipcluster'
-  end
+ CephAdminNode << array['node']
  end
 end
+
+
 
 Vagrant.configure("2") do |config|
 
@@ -26,41 +24,32 @@ Vagrant.configure("2") do |config|
 	config.hostmanager.manage_guest = true
 	config.hostmanager.include_offline = false
 	config.hostmanager.ignore_private_ip = false
-
-    cluster.each do |array|
-      
+     
+    
+     cluster.each do |array|
+       
 	   config.vm.define array['node'] do |node|   		    
 		   node.vm.hostname =  array['node']     
-
-			array['network'].each do |net|
-			    node.vm.network :private_network, 
-			        :ip => net['ipcluster'],
-			        :libvirt__dhcp_enabled => "false",
-			        :libvirt__guest_ipv6 => "no",
-			        :libvirt__forward_mode => "none",
-			        :libvirt__network_name => settings["cluster_network_name"],
-			        :libvirt__netmask => settings["cluster_netmask"]
-		    end    
-		        
-		        
+		    
+		    node.vm.network :private_network, 
+		        :ip => array['ip_cluster'],
+		        :libvirt__dhcp_enabled => "false",
+		        :libvirt__forward_mode => "none",
+		        :libvirt__network_name => settings["cluster_network_name"],
+		        :libvirt__netmask => settings["cluster_netmask"]
+		    
 		    node.vm.provider :libvirt do |v|
 		     v.cpus = 1
-		     v.memory = 512
+		     v.memory = 1024
 		     v.nested = true
 			 v.keymap = "es"
 			 v.volume_cache = "none"
 			 array['osd'].each do |o|
 			  v.storage :file, :size => '20G', :cache => 'none'
 			 end
-		    end
+		    end      
 		    
-		    node.vm.provision :hostmanager
-		  		    
-			node.vm.provision "ansible" do |an|
-			 an.playbook = "ansible/playbooks/setenv.yml"
-			 an.sudo = true
-			end	   
-	
+		    
 		end
 	end
 end		
